53f9f4c2dc782b8cc61d067be22150c790951fdf
Gui-dev <adrianon10@gmail.com>
2021-12-11 04:00:21 -0300

    feat: add model Category

---
+++ src/app/modules/categories/infra/typeorm/model/Category.ts
@@ -0,0 +1,14 @@
+import { v4 as uuid } from 'uuid'
+
+export class Category {
+  public readonly id: string
+  public readonly name: string
+  public readonly description: string
+  public readonly created_at: Date
+
+  constructor () {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
