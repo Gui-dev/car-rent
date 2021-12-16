45603a1321a5868985e0dd4a7c9b13defb388bfa
Gui-dev <adrianon10@gmail.com>
2021-12-16 03:07:20 -0300

    feat: add helper method index

--- src/app/modules/categories/infra/http/controllers/CategoryController.ts
+++ src/app/modules/categories/infra/http/controllers/CategoryController.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express'
 
-import { CategoryRepository } from '@modules/categories/infra/typeorm/repositories/CategoryRepository'
 import { CreateCategoryService } from '@modules/categories/services/CreateCategoryService'
+import { ListCategoriesService } from '@modules/categories/services/ListCategoriesService'
+import { CategoryRepository } from '../../typeorm/repositories/CategoryRepository'
 
 export class CategoryController {
+  // constructor (
+  //   private createCategoryService: CreateCategoryService,
+  //   private listCategoriesService: ListCategoriesService
+  // ) {}
+
+  public async index (request: Request, response: Response): Promise<Response> {
+    const categoriesRepository = CategoryRepository.getInstance()
+    const listCategoriesService = new ListCategoriesService(categoriesRepository)
+    const categories = await listCategoriesService.execute()
+
+    return response.status(201).json(categories)
+  }
+
   public async create (request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body
-    const categoriesRepository = new CategoryRepository()
+    const categoriesRepository = CategoryRepository.getInstance()
     const createCategoryService = new CreateCategoryService(categoriesRepository)
-
     const categories = await createCategoryService.execute({ name, description })
 
     return response.status(201).json(categories)
