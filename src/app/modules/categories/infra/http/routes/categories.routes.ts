c69b9657ee6ebbff1e7d86feedf470d37d40a69c
Gui-dev <adrianon10@gmail.com>
2021-12-16 03:07:56 -0300

    refactor: update routes

--- src/app/modules/categories/infra/http/routes/categories.routes.ts
+++ src/app/modules/categories/infra/http/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from 'express'
 
 import { CategoryController } from '@modules/categories/infra/http/controllers/CategoryController'
-import { CategoryRepository } from '../../typeorm/repositories/CategoryRepository'
 
 const categoriesRouter = Router()
-
 const categoryController = new CategoryController()
-const categoriesRepository = new CategoryRepository()
 
 categoriesRouter.post('/', categoryController.create)
 
-categoriesRouter.get('/', async (req, res) => {
-  const categories = await categoriesRepository.listCategories()
-  return res.status(201).json(categories)
-})
+categoriesRouter.get('/', categoryController.index)
 
 export {
   categoriesRouter
