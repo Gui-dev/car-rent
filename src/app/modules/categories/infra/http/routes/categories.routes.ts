5c3421b56ca94334da9ad46da49453e55dbaa6a6
Gui-dev <adrianon10@gmail.com>
2021-12-17 04:05:29 -0300

    feat: add new route

--- src/app/modules/categories/infra/http/routes/categories.routes.ts
+++ src/app/modules/categories/infra/http/routes/categories.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { CategoryController } from '@modules/categories/infra/http/controllers/CategoryController'
+import { ImportController } from '@modules/categories/infra/http/controllers/ImportController'
+import { uploadMulter } from '@config/uploadMulter'
 
 const categoriesRouter = Router()
 const categoryController = new CategoryController()
+const importCategory = new ImportController()
+
+const upload = multer(uploadMulter)
 
 categoriesRouter.post('/', categoryController.create)
+
+categoriesRouter.post('/import', upload.single('file'), importCategory.create)
 
 categoriesRouter.get('/', categoryController.index)
 
