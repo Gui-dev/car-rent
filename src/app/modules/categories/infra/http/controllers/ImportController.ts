aa7c3ac76bcb9462818a69d960b399cbc084c2f5
Gui-dev <adrianon10@gmail.com>
2021-12-18 03:44:53 -0300

    feat: add implementing the create method

--- src/app/modules/categories/infra/http/controllers/ImportController.ts
+++ src/app/modules/categories/infra/http/controllers/ImportController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from 'express'
 
 import { ImportCategoryService } from '@modules/categories/services/ImportCategoryService'
+import { CategoryRepository } from '../../typeorm/repositories/CategoryRepository'
 
 export class ImportController {
   public async create (request: Request, response: Response): Promise<Response> {
     const { file } = request
-    const importCategoryService = new ImportCategoryService()
-    const importCategory = importCategoryService.execute({ file })
+    const categoryRepository = CategoryRepository.getInstance()
+    const importCategoryService = new ImportCategoryService(categoryRepository)
+    const importCategory = await importCategoryService.execute({ file })
 
     return response.status(201).json(importCategory)
   }
