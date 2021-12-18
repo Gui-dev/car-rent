bffd180b12663b60ae0f124701813ae99391c27a
Gui-dev <adrianon10@gmail.com>
2021-12-18 03:43:21 -0300

    feat: add helper methods execute and loadCategoriesFile

--- src/app/modules/categories/services/ImportCategoryService.ts
+++ src/app/modules/categories/services/ImportCategoryService.ts
@@ -1,13 +1,62 @@
 import { Express } from 'express'
+import { createReadStream } from 'fs'
+import { parse as csvParse } from 'csv-parse'
+
+import { ICategoryRepository } from '../repositories/ICategoryRepository'
+import { AppError } from '@shared/infra/error/AppError'
 
 type ImportCategoryServiceProps = {
   file: Express.Multer.File
 }
 
-export class ImportCategoryService {
-  constructor () {}
+type loadCategoriesFileProps = {
+  name: string
+  description: string
+}
 
-  public async execute ({ file }: ImportCategoryServiceProps) {
-    console.log(file)
+export class ImportCategoryService {
+  constructor (private categoriesRepository: ICategoryRepository) {}
+
+  public async execute ({ file }: ImportCategoryServiceProps): Promise<void> {
+    const categories = await this.loadCategoriesFile({ file })
+
+    categories.map(async (category) => {
+      const { name, description } = category
+      const existsCategory = await this.categoriesRepository.findByName(name)
+
+      if (!existsCategory) {
+        await this.categoriesRepository.create({
+          name,
+          description
+        })
+      }
+    })
+  }
+
+  private async loadCategoriesFile ({ file }: ImportCategoryServiceProps): Promise<loadCategoriesFileProps[]> {
+    return new Promise((resolve, reject) => {
+      const categories: loadCategoriesFileProps[] = []
+      const stream = createReadStream(file.path)
+      const parseFile = csvParse()
+
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', (line) => {
+          const [name, description] = line
+          categories.push({
+            name,
+            description
+          })
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', () => {
+          reject(new AppError('Error uploading file', 400))
+        })
+
+      return categories
+    })
   }
 }
