41d546154bab3f80fa9eebe4a7909ce3335ec9ec
Gui-dev <adrianon10@gmail.com>
2021-12-28 03:39:46 -0300

    feat: add access to repository Category

--- src/app/modules/categories/infra/typeorm/repositories/CategoryRepository.ts
+++ src/app/modules/categories/infra/typeorm/repositories/CategoryRepository.ts
@@ -1,49 +1,39 @@
-import { v4 as uuid } from 'uuid'
+import { getRepository, Repository } from 'typeorm'
 
 import { ICategoryRepository } from '@modules/categories/repositories/ICategoryRepository'
 import { Category } from '../model/Category'
+import { AppError } from '@shared/infra/error/AppError'
 
 export class CategoryRepository implements ICategoryRepository {
-  private categories: Category[]
+  private categoryRepository: Repository<Category>
 
-  private static INSTANCE: CategoryRepository
-
-  private constructor () {
-    this.categories = []
-  }
-
-  public static getInstance (): CategoryRepository {
-    if (!CategoryRepository.INSTANCE) {
-      CategoryRepository.INSTANCE = new CategoryRepository()
-    }
-
-    return CategoryRepository.INSTANCE
+  constructor () {
+    this.categoryRepository = getRepository(Category)
   }
 
   public async findByName (name: string): Promise<Category> {
-    const category = this.categories.find(category => category.name === name)
+    const category = await this.categoryRepository.findOne({ name })
 
-    if (!category) {
-      return category
+    if (category) {
+      throw new AppError('Category already exists')
     }
 
-    throw new Error('Category already exists')
+    return category
   }
 
   public async listCategories (): Promise<Category[]> {
-    return this.categories
+    const categories = await this.categoryRepository.find()
+
+    return categories
   }
 
-  public async create ({ name, description }): Promise<Category[]> {
-    const category: Category = {
-      id: uuid(),
+  public async create ({ name, description }): Promise<Category> {
+    const category = this.categoryRepository.create({
       name,
-      description,
-      created_at: new Date()
-    }
+      description
+    })
+    await this.categoryRepository.save(category)
 
-    this.categories.push(category)
-
-    return this.categories
+    return category
   }
 }
