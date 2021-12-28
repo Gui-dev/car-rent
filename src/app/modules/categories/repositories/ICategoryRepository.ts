66f6cc7ce7045ea56135275946fa05cc5ae6f324
Gui-dev <adrianon10@gmail.com>
2021-12-28 03:37:18 -0300

    refactor: remove array return

--- src/app/modules/categories/repositories/ICategoryRepository.ts
+++ src/app/modules/categories/repositories/ICategoryRepository.ts
@@ -3,6 +3,6 @@ import { Category } from '../infra/typeorm/model/Category'
 
 export interface ICategoryRepository {
   listCategories(): Promise<Category[]>
-  create(data: ICreateCategoryDTO): Promise<Category[]>
+  create(data: ICreateCategoryDTO): Promise<Category>
   findByName(name: string): Promise<Category>
 }
