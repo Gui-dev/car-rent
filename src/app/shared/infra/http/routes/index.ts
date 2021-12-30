0ce246f38d445e09da1e20a9e7dc3bf20d3fa2e5
Gui-dev <adrianon10@gmail.com>
2021-12-30 18:41:30 -0300

    feat: add new route /users

--- src/app/shared/infra/http/routes/index.ts
+++ src/app/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
+import { usersRoutes } from '@modules/users/infra/http/routes/user.routes'
 import { categoriesRouter } from '@modules/categories/infra/http/routes/categories.routes'
 import { specifcationsRouter } from '@modules/specifications/infra/http/routes/specifications.routes'
 
 const routes = Router()
 
+routes.use('/users', usersRoutes)
 routes.use('/categories', categoriesRouter)
 routes.use('/specifications', specifcationsRouter)
 
