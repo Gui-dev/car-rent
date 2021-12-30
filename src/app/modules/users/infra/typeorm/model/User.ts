407ceb3f346d8b98b001355d538b44b1c5616d04
Gui-dev <adrianon10@gmail.com>
2021-12-30 03:36:54 -0300

    feat: add model User

---
+++ src/app/modules/users/infra/typeorm/model/User.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  public id: string
+
+  @Column()
+  public name: string
+
+  @Column()
+  public username: string
+
+  @Column()
+  public email: string
+
+  @Column()
+  public password: string
+
+  @Column()
+  public driver_license: string
+
+  @Column()
+  public admin: boolean
+
+  @CreateDateColumn()
+  public create_at: Date
+
+  constructor () {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
