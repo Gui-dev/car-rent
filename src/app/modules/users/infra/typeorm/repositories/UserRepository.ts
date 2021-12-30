f95abcc09e9055455e9b200a9f8be30743de826c
Gui-dev <adrianon10@gmail.com>
2021-12-30 03:40:39 -0300

    feat: add repository UserRepository

---
+++ src/app/modules/users/infra/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from 'typeorm'
+
+import { ICreateUserDTO } from '@modules/users/dtos/ICreateUserDTO'
+import { IUserRepository } from '@modules/users/repositories/IUserRepository'
+import { User } from '@modules/users/infra/typeorm/model/User'
+
+export class UserRepository implements IUserRepository {
+  private userRepository: Repository<User>
+
+  constructor () {
+    this.userRepository = getRepository(User)
+  }
+
+  public async create ({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<User> {
+    const user = this.userRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license
+    })
+
+    await this.userRepository.save(user)
+
+    return user
+  }
+}
