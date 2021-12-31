a1cd549cf727f7af43a5107e3de5a1993497f953
Gui-dev <adrianon10@gmail.com>
2021-12-31 03:53:43 -0300

    feat: add new provider

--- src/app/modules/users/providers/index.ts
+++ src/app/modules/users/providers/index.ts
@@ -3,4 +3,8 @@ import { container } from 'tsyringe'
 import { IUserRepository } from '@modules/users/repositories/IUserRepository'
 import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
 
+import { IBcryptHashProvider } from '@modules/users/providers/hashProvider/models/IBcryptHashProvider'
+import { BcryptHashProvider } from '@modules/users/providers/hashProvider/implementations/BcryptHasProvider'
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+container.registerSingleton<IBcryptHashProvider>('BcryptHashProvider', BcryptHashProvider)
