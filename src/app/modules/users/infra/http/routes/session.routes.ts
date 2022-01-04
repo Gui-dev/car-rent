b99997c44425ffbb3af70bd9f8651f83033013a3
Gui-dev <adrianon10@gmail.com>
2022-01-04 17:47:29 -0300

    refactor: update routes

---
+++ src/app/modules/users/infra/http/routes/session.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+import { SessionController } from '@modules/users/infra/http/controllers/SessionController'
+
+const sessionsRoutes = Router()
+const sessionController = new SessionController()
+
+sessionsRoutes.post('/', sessionController.create)
+
+export { sessionsRoutes }
--- src/app/shared/infra/http/routes/index.ts
+++ src/app/shared/infra/http/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 
 import { usersRoutes } from '@modules/users/infra/http/routes/user.routes'
+import { sessionsRoutes } from '@modules/users/infra/http/routes/session.routes'
 import { categoriesRouter } from '@modules/categories/infra/http/routes/categories.routes'
 import { specifcationsRouter } from '@modules/specifications/infra/http/routes/specifications.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
+
 routes.use('/categories', categoriesRouter)
 routes.use('/specifications', specifcationsRouter)
 
