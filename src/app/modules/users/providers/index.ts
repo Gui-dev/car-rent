9a411830368f2b8fd56f60d07a03967c852a5562
Gui-dev <adrianon10@gmail.com>
2022-01-04 17:45:45 -0300

    refactor: update providers

--- src/app/modules/users/providers/index.ts
+++ src/app/modules/users/providers/index.ts
@@ -6,5 +6,9 @@ import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRe
 import { IBcryptHashProvider } from '@modules/users/providers/hashProvider/models/IBcryptHashProvider'
 import { BcryptHashProvider } from '@modules/users/providers/hashProvider/implementations/BcryptHasProvider'
 
+import { IJsonWebTokenProvider } from '@modules/users/providers/jwt/models/IJsonWebTokenProvider'
+import { JsonWebTokenProvider } from '@modules/users/providers/jwt/implementations/JsonWebTokenProvider'
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
 container.registerSingleton<IBcryptHashProvider>('BcryptHashProvider', BcryptHashProvider)
+container.registerSingleton<IJsonWebTokenProvider>('JsonWebTokenProvider', JsonWebTokenProvider)
