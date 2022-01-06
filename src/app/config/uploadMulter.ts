abdca93edff56d16caff5368c3e6796e43df683d
Gui-dev <adrianon10@gmail.com>
2022-01-06 03:55:42 -0300

    refactor: add multer folder config

--- src/app/config/uploadMulter.ts
+++ src/app/config/uploadMulter.ts
@@ -3,22 +3,21 @@ import multer from 'multer'
 import { resolve } from 'path'
 import crypto from 'crypto'
 
-const tmpFolder = resolve(__dirname, '..', '..', '..', 'tmp')
-const uploadFolder = resolve(__dirname, '..', '..', '..', 'tmp', 'uploads')
+export default {
+  upload (folder: string) {
+    const tmpFolder = resolve(__dirname, '..', '..', '..', 'tmp', folder)
+    return {
+      storage: multer.diskStorage({
+        destination: (request: Request, file: Express.Multer.File, callback) => {
+          return callback(null, tmpFolder)
+        },
+        filename: (request: Request, file: Express.Multer.File, callback) => {
+          const fileHash = crypto.randomBytes(10).toString('hex')
+          const filename = `${fileHash}&${file.originalname}`
 
-const storage = multer.diskStorage({
-  destination: (request: Request, file: Express.Multer.File, callback) => {
-    return callback(null, uploadFolder)
-  },
-  filename: (request: Request, file: Express.Multer.File, callback) => {
-    const fileHash = crypto.randomBytes(10).toString('hex')
-    const filename = `${fileHash}&${file.originalname}`
-
-    return callback(null, filename)
+          return callback(null, filename)
+        }
+      })
+    }
   }
-})
-
-export const uploadMulter = {
-  dest: tmpFolder,
-  storage
 }
