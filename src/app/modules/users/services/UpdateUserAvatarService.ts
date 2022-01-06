597cba5379fba485ae85f4395554b1c773e6d124
Gui-dev <adrianon10@gmail.com>
2022-01-06 03:44:09 -0300

    feat: add service UpdateUserAvatarService

---
+++ src/app/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe'
+
+import { IUpdateUserAvatarDTO } from '../dtos/IUpdateUserAvatarDTO'
+import { IUserRepository } from '../repositories/IUserRepository'
+import { AppError } from '@shared/infra/error/AppError'
+import { User } from '../infra/typeorm/model/User'
+
+@injectable()
+export class UpdateUserAvatarService {
+  constructor (
+    @inject('UserRepository')
+    private userRepository: IUserRepository
+  ) {}
+
+  public async execute ({ user_id, avatar_file }: IUpdateUserAvatarDTO): Promise<User> {
+    const user = await this.userRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('User not found', 401)
+    }
+
+    user.avatar = avatar_file
+    await this.userRepository.create(user)
+
+    return user
+  }
+}
