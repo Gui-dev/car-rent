38dd3ffaabef06ed7051f0c58b1b33b6bc24ab4b
Gui-dev <adrianon10@gmail.com>
2022-01-06 03:58:08 -0300

    feat: add new route

--- src/app/modules/users/infra/http/routes/user.routes.ts
+++ src/app/modules/users/infra/http/routes/user.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { UserController } from '@modules/users/infra/http/controllers/UserController'
+import { UpdateUserAvatarController } from '@modules/users/infra/http/controllers/UpdateUserAvatarController'
+import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
+import multerConfig from '@config/uploadMulter'
 
 const usersRoutes = Router()
 const userController = new UserController()
+const updateUserAvatarController = new UpdateUserAvatarController()
+
+const upload = multer(multerConfig.upload('avatar'))
 
 usersRoutes.post('/', userController.create)
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  updateUserAvatarController.create
+)
 
 export { usersRoutes }
