b3d0fff3e959bb6b2e7dc041d0711fc837b69920
Gui-dev <adrianon10@gmail.com>
2022-01-13 00:27:14 -0300

    test: should be able to create a new car, and should not be able to create a car with exists license plate

---
+++ src/app/modules/cars/services/__tests__/CreateCarService.spec.ts
@@ -0,0 +1,55 @@
+import { CreateCarService } from '../CreateCarService'
+import { CarRepositoryInMemory } from '@modules/cars/infra/typeorm/in-memory/CarRepositoryInMemory'
+import { AppError } from '@shared/infra/error/AppError'
+
+let createCarService: CreateCarService
+let carRepositoryInMemory: CarRepositoryInMemory
+
+describe('Create Car Service', () => {
+  beforeEach(() => {
+    carRepositoryInMemory = new CarRepositoryInMemory()
+    createCarService = new CreateCarService(carRepositoryInMemory)
+  })
+
+  it('should be able to create a new car', async () => {
+    const car = {
+      category_id: 'fake_category_id',
+      name: 'fake_name',
+      description: 'fake_description',
+      daily_rate: 100,
+      license_plate: 'fake_plate',
+      fine_amount: 60,
+      brand: 'fake_brand'
+    }
+    const response = await createCarService.execute(car)
+
+    expect(response).toHaveProperty('id')
+  })
+
+  it('should not be able to create a car with exists license plate', () => {
+    expect(async () => {
+      const car = {
+        category_id: 'fake_category_id',
+        name: 'fake_name',
+        description: 'fake_description',
+        daily_rate: 100,
+        license_plate: 'fake_plate',
+        fine_amount: 60,
+        brand: 'fake_brand'
+      }
+
+      const newCar = {
+        category_id: 'fake_category_id_2',
+        name: 'fake_name',
+        description: 'fake_description',
+        daily_rate: 100,
+        license_plate: 'fake_plate',
+        fine_amount: 60,
+        brand: 'fake_brand'
+      }
+
+      await createCarService.execute(car)
+      await createCarService.execute(newCar)
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
