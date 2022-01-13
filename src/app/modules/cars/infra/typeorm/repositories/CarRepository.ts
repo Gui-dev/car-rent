0e2ed430224accc0eb178ccc6d81a59e5b857bb5
Gui-dev <adrianon10@gmail.com>
2022-01-13 03:58:20 -0300

    feat: add helpers methods create and findByLicensePlate

--- src/app/modules/cars/infra/typeorm/repositories/CarRepository.ts
+++ src/app/modules/cars/infra/typeorm/repositories/CarRepository.ts
@@ -1,9 +1,26 @@
+import { getRepository, Repository } from 'typeorm'
+
 import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO'
 import { ICarRepository } from '@modules/cars/repositories/ICarRepository'
 import { Car } from '../model/Car'
 
 export class CarRepository implements ICarRepository {
+  private repositoryCar: Repository<Car>
+
+  constructor () {
+    this.repositoryCar = getRepository(Car)
+  }
+
   public async create (data: ICreateCarDTO): Promise<Car> {
-    throw new Error('Method not implemented.')
+    const car = this.repositoryCar.create(data)
+    await this.repositoryCar.save(car)
+
+    return car
+  }
+
+  public async findByLicensePlate (license_plate: string): Promise<Car> {
+    const car = await this.repositoryCar.findOne({ license_plate })
+
+    return car
   }
 }
