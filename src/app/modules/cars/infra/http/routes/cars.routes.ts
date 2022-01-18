642583fc6772ecbd65ef47172f587d95c11ae54d
Gui-dev <adrianon10@gmail.com>
2022-01-18 03:26:42 -0300

    feat: add new route

--- src/app/modules/cars/infra/http/routes/cars.routes.ts
+++ src/app/modules/cars/infra/http/routes/cars.routes.ts
@@ -7,6 +7,7 @@ import { ensureAdmin } from '@modules/users/infra/http/middlewares/ensureAdmin'
 const carsRouter = Router()
 const carController = new CarController()
 
+carsRouter.get('/availables', carController.index)
 carsRouter.post('/', ensureAuthenticated, ensureAdmin, carController.create)
 
 export {
