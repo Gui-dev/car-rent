4907cf66149627e0e7b4992b12c77ff619e58050
Gui-dev <adrianon10@gmail.com>
2022-01-19 03:55:30 -0300

    feat: add interface helper method findByIds

--- src/app/modules/specifications/repositories/ISpecificationsRepository.ts
+++ src/app/modules/specifications/repositories/ISpecificationsRepository.ts
@@ -4,4 +4,5 @@ import { Specification } from '../infra/typeorm/model/Specification'
 export interface ISpecificationsRepository {
   create (data: ICreateSpecificationDTO): Promise<Specification>
   findByName(name: string): Promise<Specification>
+  findByIds(specification_ids: Array<string>): Promise<Specification[]>
 }
