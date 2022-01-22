5f159503ed64eb069e2548e9cde4dec54d45a643
Gui-dev <adrianon10@gmail.com>
2022-01-22 04:06:51 -0300

    feat: add new provider

--- src/app/modules/cars/providers/index.ts
+++ src/app/modules/cars/providers/index.ts
@@ -3,4 +3,8 @@ import { container } from 'tsyringe'
 import { ICarRepository } from '@modules/cars/repositories/ICarRepository'
 import { CarRepository } from '@modules/cars/infra/typeorm/repositories/CarRepository'
 
+import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository'
+import { CarImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarImagesRepository'
+
 container.registerSingleton<ICarRepository>('CarRepository', CarRepository)
+container.registerSingleton<ICarImagesRepository>('CarImagesRepository', CarImagesRepository)
