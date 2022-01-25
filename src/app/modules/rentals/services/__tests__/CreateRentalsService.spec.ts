5bb524f3ad2e4e21b96df5ab98a3b9de5668573d
Gui-dev <adrianon10@gmail.com>
2022-01-25 03:52:16 -0300

    test: should not be able to create a new rental if there\'s another open to the same user

--- src/app/modules/rentals/services/__tests__/CreateRentalsService.spec.ts
+++ src/app/modules/rentals/services/__tests__/CreateRentalsService.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from '@modules/rentals/infra/typeorm/in-memory/RentalsRepositoryInMemory'
+import { AppError } from '@shared/infra/error/AppError'
 import { CreateRentalsService } from '../CreateRentalsService'
 
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory
@@ -18,5 +19,21 @@ describe('Create Rental Service', () => {
 
     expect(rental).toHaveProperty('id')
     expect(rental).toHaveProperty('start_date')
+  })
+
+  it('should not be able to create a new rental if there\'s another open to the same user', () => {
+    expect(async () => {
+      await createRentalsService.execute({
+        user_id: 'fake_user_id',
+        car_id: 'fake_car_id',
+        expected_return_date: new Date()
+      })
+
+      await createRentalsService.execute({
+        user_id: 'fake_user_id',
+        car_id: 'fake_car_id_2',
+        expected_return_date: new Date()
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
