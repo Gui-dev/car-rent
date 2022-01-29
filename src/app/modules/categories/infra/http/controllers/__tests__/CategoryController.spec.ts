0081eef9291cbe50fdbbb6e3d41f83389ddcf808
Gui-dev <adrianon10@gmail.com>
2022-01-29 03:39:12 -0300

    test: should be able to create a new category

---
+++ src/app/modules/categories/infra/http/controllers/__tests__/CategoryController.spec.ts
@@ -0,0 +1,51 @@
+import request from 'supertest'
+import { Connection } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+import { app } from '@shared/infra/http/App'
+import createConnection from '@shared/infra/database'
+import { BcryptHashProvider } from '@modules/users/providers/hashProvider/implementations/BcryptHasProvider'
+
+let connection: Connection
+
+describe('CategoryController', () => {
+  beforeAll(async () => {
+    connection = await createConnection()
+    await connection.runMigrations()
+
+    const bcryptHashProvider = new BcryptHashProvider()
+    const password = await bcryptHashProvider.generateHash('123456')
+    const id = uuid()
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, driver_license, "admin", created_at)
+      VALUES('${id}', 'Batman', 'batman@email.com', '${password}', 'XXXXXXXXX', 'true', 'now()')
+      `
+    )
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase()
+    await connection.close()
+  })
+
+  it('should be able to create a new category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'batman@email.com',
+      password: '123456'
+    })
+
+    const { token } = responseToken.body
+
+    const response = await request(app).post('/categories')
+      .send({
+        name: 'fake_name',
+        description: 'fake_description'
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      })
+
+    expect(response.status).toBe(201)
+  })
+})
