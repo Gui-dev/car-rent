e7117f44584bd8a6b00e083fd2db5bc89b3ff522
Gui-dev <adrianon10@gmail.com>
2022-02-01 02:11:40 -0300

    feat: add update car status

--- src/app/modules/rentals/services/CreateRentalsService.ts
+++ src/app/modules/rentals/services/CreateRentalsService.ts
@@ -4,6 +4,7 @@ import { IRentalsRepository } from '../repositories/IRentalsRepository'
 import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO'
 import { AppError } from '@shared/infra/error/AppError'
 import { Rental } from '../infra/typeorm/model/Rental'
+import { ICarRepository } from '@modules/cars/repositories/ICarRepository'
 import { IDateProvider } from '../providers/DateProvider/models/IDateProvider'
 
 @injectable()
@@ -13,6 +14,8 @@ export class CreateRentalsService {
   constructor (
     @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
+    @inject('CarRepository')
+    private carRepository: ICarRepository,
     @inject('DayJSDateProvider')
     private dayJSDateProvider: IDateProvider
   ) {}
@@ -42,6 +45,8 @@ export class CreateRentalsService {
       car_id,
       expected_return_date
     })
+
+    await this.carRepository.updateAvailable(car_id, false)
 
     return rentalCar
   }
