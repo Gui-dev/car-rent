33872110fab89a7045fa394bd91c972a13573648
Gui-dev <adrianon10@gmail.com>
2022-02-02 03:26:22 -0300

    refactor: add rename controller

--- src/app/modules/rentals/infra/http/routes/rentals.routes.ts
+++ src/app/modules/rentals/infra/http/routes/rentals.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
-import { CreateRentalController } from '@modules/rentals/infra/http/controllers/CreateRentalController'
+import { RentalController } from '@modules/rentals/infra/http/controllers/RentalController'
 import { DevolutionRentalController } from '@modules/rentals/infra/http/controllers/DevolutionRentalController'
 
 const rentalsRoutes = Router()
-const createRentalController = new CreateRentalController()
+const rentalController = new RentalController()
 const devolutionRentalController = new DevolutionRentalController()
 
-rentalsRoutes.post('/:car_id', ensureAuthenticated, createRentalController.create)
+rentalsRoutes.get('/', ensureAuthenticated, rentalController.index)
+rentalsRoutes.post('/:car_id', ensureAuthenticated, rentalController.create)
 rentalsRoutes.post('/devolutions/:rental_id', ensureAuthenticated, devolutionRentalController.create)
 
 export {
