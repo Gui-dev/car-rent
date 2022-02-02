20b0d8577def5f264976e9df0d836ce0c8607379
Gui-dev <adrianon10@gmail.com>
2022-02-02 01:58:35 -0300

    feat: add new route

--- src/app/modules/rentals/infra/http/routes/rentals.routes.ts
+++ src/app/modules/rentals/infra/http/routes/rentals.routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import { CreateRentalController } from '@modules/rentals/infra/http/controllers/CreateRentalController'
+import { DevolutionRentalController } from '@modules/rentals/infra/http/controllers/DevolutionRentalController'
 
 const rentalsRoutes = Router()
 const createRentalController = new CreateRentalController()
+const devolutionRentalController = new DevolutionRentalController()
 
 rentalsRoutes.post('/:car_id', ensureAuthenticated, createRentalController.create)
+rentalsRoutes.post('/devolutions/:rental_id', ensureAuthenticated, devolutionRentalController.create)
 
 export {
   rentalsRoutes
