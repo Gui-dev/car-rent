a94f2a321eb1e9dcc95d48ef7250a4445253e618
Gui-dev <adrianon10@gmail.com>
2022-02-02 01:51:55 -0300

    feat: add helper method interface findById

--- src/app/modules/rentals/repositories/IRentalsRepository.ts
+++ src/app/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,4 +5,5 @@ export interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>
   findOpenRentalByCar(car_id: string): Promise<Rental | undefined>
   findOpenRentalByUser(user_id: string): Promise<Rental | undefined>
+  findById(rental_id: string): Promise<Rental | undefined>
 }
