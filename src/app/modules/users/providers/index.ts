ad217318497adbd4d0e45121c9ee42dd8f7dd3a4
Gui-dev <adrianon10@gmail.com>
2022-02-07 03:41:57 -0300

    refactor: moving folder files

--- src/app/modules/users/providers/index.ts
+++ src/app/modules/users/providers/index.ts
@@ -3,8 +3,12 @@ import { container } from 'tsyringe'
 import { IUserRepository } from '@modules/users/repositories/IUserRepository'
 import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
 
+import { IUsersTokenRepository } from '@modules/users/repositories/IUsersTokenRepository'
+import { UsersTokenRepository } from '@modules/users/infra/typeorm/repositories/UsersTokenRepository'
+
 import { IBcryptHashProvider } from '@modules/users/providers/hashProvider/models/IBcryptHashProvider'
 import { BcryptHashProvider } from '@modules/users/providers/hashProvider/implementations/BcryptHasProvider'
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+container.registerSingleton<IUsersTokenRepository>('UsersTokenRepository', UsersTokenRepository)
 container.registerSingleton<IBcryptHashProvider>('BcryptHashProvider', BcryptHashProvider)
