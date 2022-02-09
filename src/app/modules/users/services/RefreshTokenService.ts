0558b0c3b016a4bf14195ac46b85d572ef05d12a
Gui-dev <adrianon10@gmail.com>
2022-02-09 01:18:22 -0300

    feat: add service RefreshTokenService

---
+++ src/app/modules/users/services/RefreshTokenService.ts
@@ -0,0 +1,50 @@
+import { inject, injectable } from 'tsyringe'
+import { verify, sign } from 'jsonwebtoken'
+
+import { IUsersTokenRepository } from '../repositories/IUsersTokenRepository'
+import { authConfig } from '@config/auth'
+import { AppError } from '@shared/infra/error/AppError'
+import { IDateProvider } from '@shared/providers/DateProvider/models/IDateProvider'
+import { UserToken } from '../infra/typeorm/model/UserToken'
+
+type IPayload = {
+  sub: string
+  email: string
+}
+
+@injectable()
+export class RefreshTokenService {
+  constructor (
+    @inject('UsersTokenRepository')
+    private usersTokenRepository: IUsersTokenRepository,
+    @inject('DayJSDateProvider')
+    private dayJSDateProvider: IDateProvider
+  ) {}
+
+  public async execute (refresh_token: string): Promise<UserToken> {
+    const { email, sub } = verify(refresh_token, authConfig.refresh_token) as IPayload
+    const userId = sub
+    const userTokens = await this.usersTokenRepository.findByUserIdAndRefreshToken(userId, refresh_token)
+
+    if (!userTokens) {
+      throw new AppError('Refresh token doesn\'t exists')
+    }
+
+    await this.usersTokenRepository.deleteById(userTokens.id)
+
+    const newRefreshToken = sign({ email }, authConfig.refresh_token, {
+      subject: sub,
+      expiresIn: authConfig.refreshTokenExpiresIn
+    })
+
+    const expiresRefreshTokenDays = this.dayJSDateProvider.addDays(authConfig.expiresRefreshTokenDays)
+
+    const token = await this.usersTokenRepository.create({
+      user_id: sub,
+      refresh_token: newRefreshToken,
+      expires_date: expiresRefreshTokenDays
+    })
+
+    return token
+  }
+}
