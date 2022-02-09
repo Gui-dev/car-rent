730ab5cc3f0c5d6840e905517e7daf585f5abc04
Gui-dev <adrianon10@gmail.com>
2022-02-09 01:21:34 -0300

    feat: add new route

--- src/app/modules/users/infra/http/routes/session.routes.ts
+++ src/app/modules/users/infra/http/routes/session.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 
 import { SessionController } from '@modules/users/infra/http/controllers/SessionController'
+import { RefreshTokenController } from '@modules/users/infra/http/controllers/RefreshTokenController'
 
 const sessionsRoutes = Router()
 const sessionController = new SessionController()
+const refreshTokenController = new RefreshTokenController()
 
 sessionsRoutes.post('/', sessionController.create)
+sessionsRoutes.post('/refresh-tokens', refreshTokenController.create)
 
 export { sessionsRoutes }
