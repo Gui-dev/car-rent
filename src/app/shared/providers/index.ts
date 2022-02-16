15261b592cc555d8f1522a0be39e246181f7dace
Gui-dev <adrianon10@gmail.com>
2022-02-16 04:10:29 -0300

    feat: add new provider

--- src/app/shared/providers/index.ts
+++ src/app/shared/providers/index.ts
@@ -6,5 +6,17 @@ import { DayJSDateProvider } from '@shared/providers/DateProvider/implementation
 import { IMailProvider } from '@shared/providers/MailProvider/models/IMailProvider'
 import { EtherealMailProvider } from '@shared/providers/MailProvider/implementations/EtherealMailProvider'
 
+import { IStorageProvider } from '@shared/providers/StorageProvider/models/IStorageProvider'
+import { LocalStorageProvider } from '@shared/providers/StorageProvider/implementations/LocalStorageProvider'
+import { AwsS3StorageProvider } from '@shared/providers/StorageProvider/implementations/AwsS3StorageProvider'
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: AwsS3StorageProvider
+}
+
 container.registerSingleton<IDateProvider>('DayJSDateProvider', DayJSDateProvider)
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider())
+container.registerSingleton<IStorageProvider>('StorageProvider',
+  process.env.APP_STATUS === 'development' ? diskStorage.local : diskStorage.s3
+)
