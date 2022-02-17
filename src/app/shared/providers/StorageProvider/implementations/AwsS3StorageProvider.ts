0f601616728b4712e809c0603e984046f4c357d2
Gui-dev <adrianon10@gmail.com>
2022-02-17 02:37:26 -0300

    feat: add provider AwsS3StorageProvider

--- src/app/shared/providers/StorageProvider/implementations/AwsS3StorageProvider.ts
+++ src/app/shared/providers/StorageProvider/implementations/AwsS3StorageProvider.ts
@@ -1,11 +1,42 @@
+import uploadMulter from '@config/uploadMulter'
+import { S3 } from 'aws-sdk'
+import { resolve } from 'path'
+import { promises } from 'fs'
+import mime from 'mime'
+
 import { IStorageProvider } from '../models/IStorageProvider'
 
-export class LocalStorageProvider implements IStorageProvider {
+export class AwsS3StorageProvider implements IStorageProvider {
+  private clientS3: S3
+
+  constructor () {
+    this.clientS3 = new S3({
+      region: process.env.AWS_BUCKET_REGION
+    })
+  }
+
   public async save (file: string, folder: string): Promise<string> {
-    throw new Error('Method not implemented.')
+    const originalName = resolve(uploadMulter.tmpFolder, file)
+    const fileContent = await promises.readFile(originalName)
+    const contentType = mime.getType(originalName) || undefined
+
+    await this.clientS3.putObject({
+      Bucket: `${process.env.AWS_BUCKET_NAME}/${folder}`,
+      Key: file,
+      Body: fileContent,
+      ContentType: contentType,
+      ACL: 'public-read'
+    }).promise()
+
+    await promises.unlink(originalName)
+
+    return file
   }
 
   public async delete (file: string, folder: string): Promise<void> {
-    throw new Error('Method not implemented.')
+    await this.clientS3.deleteObject({
+      Bucket: `${process.env.AWS_BUCKET_NAME}/${folder}`,
+      Key: file
+    }).promise()
   }
 }
