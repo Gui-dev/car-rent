ec0fd55bb5f39823c01faedd21e1b070d7117612
Gui-dev <adrianon10@gmail.com>
2022-02-18 03:22:56 -0300

    feat: add routes to files statics

--- src/app/shared/infra/http/App.ts
+++ src/app/shared/infra/http/App.ts
@@ -8,6 +8,7 @@ import { serve, setup } from 'swagger-ui-express'
 import swaggerFile from './../../../../../swagger.json'
 import '@shared/container'
 import createConnection from '@shared/infra/database'
+import uploadMulter from '@config/uploadMulter'
 import { AppError } from '../error/AppError'
 
 import { routes } from '@shared/infra/http/routes'
@@ -17,6 +18,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/avatar', express.static(`${uploadMulter.tmpFolder}/avatar`))
+app.use('/cars', express.static(`${uploadMulter.tmpFolder}/cars`))
+app.use('/files', express.static(`${uploadMulter.tmpFolder}/files`))
 
 app.use('/api-docs', serve, setup(swaggerFile))
 
