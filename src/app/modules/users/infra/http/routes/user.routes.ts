0a06520bfb395ca60e6838ee36fe9f80539e3ed5
Gui-dev <adrianon10@gmail.com>
2022-02-18 03:20:24 -0300

    feat: add new route

--- src/app/modules/users/infra/http/routes/user.routes.ts
+++ src/app/modules/users/infra/http/routes/user.routes.ts
@@ -5,6 +5,7 @@ import { UserController } from '@modules/users/infra/http/controllers/UserContro
 import { UpdateUserAvatarController } from '@modules/users/infra/http/controllers/UpdateUserAvatarController'
 import { SendForgotPasswordController } from '@modules/users/infra/http/controllers/SendForgotPasswordController'
 import { ResetPasswordController } from '@modules/users/infra/http/controllers/ResetPasswordController'
+import { ProfileUserController } from '@modules/users/infra/http/controllers/ProfileUserController'
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import multerConfig from '@config/uploadMulter'
 
@@ -13,6 +14,7 @@ const userController = new UserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
 const sendForgotPasswordController = new SendForgotPasswordController()
 const resetPasswordController = new ResetPasswordController()
+const profileUserController = new ProfileUserController()
 
 const upload = multer(multerConfig)
 
@@ -26,5 +28,6 @@ usersRoutes.patch(
   upload.single('avatar'),
   updateUserAvatarController.create
 )
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.show)
 
 export { usersRoutes }
